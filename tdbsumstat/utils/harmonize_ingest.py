ec9620144a4fb2106976aaa9b5ee7888349947d9
bruno-ariano <bruno.ariano.87@gmail.com>
2025-11-22 17:16:35 +0100

    Adding mac filter ingestion

--- tdbsumstat/utils/harmonize_ingest.py
+++ tdbsumstat/utils/harmonize_ingest.py
@@ -134,16 +134,10 @@ class Harmonize:
                 else:
                     raise HarmonizationError("N column is missing and N parameter is not provided")
             if self.mac is not None:
-                self.chunk_pl = self.chunk_pl.with_columns((pl.when(
-                    pl.col('EAF')>0.5
-                ).then(
-                    1-pl.col('EAF')
-                ).otherwise(
-                    pl.col('EAF')
-                )
-                * 2
-                * pl.col('N')).alias('MAC'))
-                self.chunk_pl = self.chunk_pl.filter(pl.col('MAC')>self.mac)
+                self.chunk_pl = self.chunk_pl.with_columns(
+                (2 * pl.col("N") * pl.min_horizontal("EAF", (1 - pl.col("EAF"))))
+                .alias("MAC")
+                 ).filter(pl.col("MAC") > self.mac)
             
         elif self.type_trait == "binary": 
             
@@ -157,16 +151,10 @@ class Harmonize:
                 else:
                     raise HarmonizationError("n_cases and n_controls columns are missing and were not provided")
                 if self.mac is not None:
-                    self.chunk_pl = self.chunk_pl.with_columns((pl.when(
-                    pl.col('EAF')>0.5
-                    ).then(
-                    1-pl.col('EAF')
-                    ).otherwise(
-                    pl.col('EAF')
-                    )
-                    * 2
-                    * pl.col('N')).alias('MAC'))
-                    self.chunk_pl = self.chunk_pl.filter(pl.col('MAC')>self.mac)
+                    self.chunk_pl = self.chunk_pl.with_columns(
+                    (2 * pl.col("N") * pl.min_horizontal("EAF", (1 - pl.col("EAF"))))
+                    .alias("MAC")
+                    ).filter(pl.col("MAC") > self.mac)
         else:
             raise HarmonizationError("Type of trait must be either binary or quant")
 
