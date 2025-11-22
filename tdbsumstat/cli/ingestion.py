3d987656a454d8df1c7e305f49f916dff3495534
bruno-ariano <bruno.ariano.87@gmail.com>
2025-11-22 16:31:06 +0100

    Adding mac filter ingestion

--- tdbsumstat/cli/ingestion.py
+++ tdbsumstat/cli/ingestion.py
@@ -21,11 +21,22 @@ import polars as pl
     "Optional parameters",
     cloup.option("--pvar-file", default = None, type=str, help = "pvar file used to verify the alleles order"),
     cloup.option("--sep", default = "\t", type=str, help = "pvar file used to verify the alleles order"),
+    cloup.option("--mac", default = "\t", type=str, help = "Allele count filter"),
     cloup.option("--qc", is_flag=True, type=bool, default = False, help = "Harmonize and QC the summary statistics using gwaslab"),
     cloup.option("--only-meta", is_flag=True, type=bool, default = False, help = "Create and ingest metadata")
 )
 
-def ingest(uri_path:str, sep:str, type_trait:str, mapping_file:str, file_path:str, create_tiledb:bool, type_sumstat:str, pvar_file:str = None, qc:bool = False, only_meta:bool = False):
+def ingest(uri_path:str, 
+           sep:str, 
+           type_trait:str, 
+           mapping_file:str, 
+           file_path:str, 
+           create_tiledb:bool, 
+           type_sumstat:str, 
+           pvar_file:str = None, 
+           qc:bool = False, 
+           only_meta:bool = False,
+           mac:int = None):
     # Create a Harmonize object
     print("Starting ingestion")
     harmonized_object = Harmonize(mapping_file= mapping_file, uri=uri_path, type_sumstat=type_sumstat, pvar_file = pvar_file, type_trait = type_trait)
@@ -74,7 +85,7 @@ def ingest(uri_path:str, sep:str, type_trait:str, mapping_file:str, file_path:st
         # Harmonize the data
         print(f"Harmonizing file: {file}")
         chunk_pl = pl.read_csv(file,separator=sep,low_memory=True ,null_values="NA")
-        harmonized_object.harmonize(sumstat = chunk_pl, trait = trait, cell = cell, gene = gene, n = n, n_cases = n_cases, n_controls = n_controls)
+        harmonized_object.harmonize(sumstat = chunk_pl, trait = trait, cell = cell, gene = gene, n = n, n_cases = n_cases, n_controls = n_controls, mac = mac)
         #Performing QC using GWASLAB
         if qc:
             harmonized_object.qc_sumstat(file_path = file)
